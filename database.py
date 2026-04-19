d27799bc1eeb6dad40527b2f61036eb4ffd8e52a
altynyi <ajtkulovaaltynaj19@gmail.com>
2026-04-19 23:47:16 +0600

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,5 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# Создаем файл базы данных blog.db
 engine = create_engine("sqlite:///blog.db")
 SessionLocal = sessionmaker(bind=engine)
